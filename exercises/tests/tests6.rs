6f3e89c03f1f332f559cc20a2e3859a9403d4e92
SongForDong <114040525+SongForDong@users.noreply.github.com>
2024-04-07 23:35:06 +0000

    update16

--- exercises/tests/tests6.rs
+++ exercises/tests/tests6.rs
@@ -7,8 +7,8 @@
 // Execute `rustlings hint tests6` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
+#[derive(Debug)]
 struct Foo {
     a: u128,
     b: Option<String>,
@@ -20,7 +20,8 @@ struct Foo {
 unsafe fn raw_pointer_to_box(ptr: *mut Foo) -> Box<Foo> {
     // SAFETY: The `ptr` contains an owned box of `Foo` by contract. We
     // simply reconstruct the box from that pointer.
-    let mut ret: Box<Foo> = unsafe { Box::from_raw(ptr) };
+    (*ptr).b = Some("hello".to_string());
+    let ret : Box<Foo> = Box::from_raw(ptr);
     ret
 }
 
@@ -40,6 +41,7 @@ mod tests {
         let ptr_2 = &ret.a as *const u128 as usize;
 
         assert!(ptr_1 == ptr_2);
+        println!("{:?}", ret);
         assert!(ret.b == Some("hello".to_owned()));
     }
 }
